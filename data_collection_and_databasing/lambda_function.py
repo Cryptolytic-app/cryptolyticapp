774bc795fdc21f3ea9fe54baa5a074394eeb5a33
nrvanwyck <49887859+nrvanwyck@users.noreply.github.com>
2019-10-29 18:06:30 -0400

    Add files via upload
    Former-commit-id: 1b90e03607c9fa892b1270cadd474f030f0c1a2d

--- data_collection_and_databasing/lambda_function.py
+++ data_collection_and_databasing/lambda_function.py
@@ -2,7 +2,15 @@ import psycopg2 as ps
 import requests
 import time
 
-# Supported trading pairs
+# defining credentials
+credentials = {'POSTGRES_ADDRESS' : '#',
+               'POSTGRES_PORT' : '#',
+               'POSTGRES_USERNAME' : '#',
+               'POSTGRES_PASSWORD' : '#',
+               'POSTGRES_DBNAME' : '#',
+               'API_KEY' : '#'}
+
+# supported trading pairs
 coinbase_pro_pairs = ['bch_btc', 'bch_usd', 'btc_usd', 'btc_usdc', 'dash_btc',
                       'dash_usd', 'eos_btc', 'eos_usd', 'etc_usd', 'eth_btc',
                       'eth_usd', 'eth_usdc', 'ltc_btc', 'ltc_usd', 'xrp_btc',
@@ -17,46 +25,10 @@ hitbtc_pairs = ['bch_btc', 'bch_usdt', 'btc_usdc', 'btc_usdt', 'dash_btc',
                 'eth_usdc', 'eth_usdt', 'ltc_btc', 'ltc_usdt', 'xrp_btc', 
                 'xrp_usdt', 'zec_usdt', 'zrx_usdt']
 
-# Supported exchanges
+# supported exchanges
 exchanges = {'bitfinex': bitfinex_pairs,
              'coinbase-pro': coinbase_pro_pairs,
              'hitbtc': hitbtc_pairs}
-
-# Decrypt credentials
-ENCRYPTED_POSTGRES_ADDRESS = os.environ['POSTGRES_ADDRESS']
-DECRYPTED_POSTGRES_ADDRESS = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_POSTGRES_ADDRESS))[
-    'Plaintext'].decode()
-
-ENCRYPTED_POSTGRES_PORT = os.environ['POSTGRES_PORT']
-DECRYPTED_POSTGRES_PORT = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_POSTGRES_PORT))[
-    'Plaintext'].decode()
-
-ENCRYPTED_POSTGRES_USERNAME = os.environ['POSTGRES_USERNAME']
-DECRYPTED_POSTGRES_USERNAME = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_POSTGRES_USERNAME))[
-    'Plaintext'].decode()
-
-ENCRYPTED_POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
-DECRYPTED_POSTGRES_PASSWORD = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_POSTGRES_PASSWORD))[
-    'Plaintext'].decode()
-
-ENCRYPTED_POSTGRES_DBNAME = os.environ['POSTGRES_DBNAME']
-DECRYPTED_POSTGRES_DBNAME = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_POSTGRES_DBNAME))[
-    'Plaintext'].decode()
-
-ENCRYPTED_POSTGRES_PORT = os.environ['POSTGRES_PORT']
-DECRYPTED_POSTGRES_PORT = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_POSTGRES_PORT))[
-    'Plaintext'].decode()
-
-ENCRYPTED_API_KEY = os.environ['API_KEY']
-DECRYPTED_API_KEY = boto3.client('kms').decrypt(CiphertextBlob=b64decode(ENCRYPTED_API_KEY))['Plaintext'].decode()
-
-# Define credentials
-credentials = {'POSTGRES_ADDRESS': DECRYPTED_POSTGRES_ADDRESS,
-               'POSTGRES_PORT': DECRYPTED_POSTGRES_PORT,
-               'POSTGRES_USERNAME': DECRYPTED_POSTGRES_USERNAME,
-               'POSTGRES_PASSWORD': DECRYPTED_POSTGRES_PASSWORD,
-               'POSTGRES_DBNAME': DECRYPTED_POSTGRES_DBNAME,
-               'API_KEY': DECRYPTED_API_KEY}
 
 def insert_data(credentials, exchanges, periods=['300','3600']):
     """This function connects to a database and inserts live data from
@@ -64,21 +36,23 @@ def insert_data(credentials, exchanges, periods=['300','3600']):
     Option to select a period ('60', '300', '3600'; default=['300', '3600'])
     """
 
-    # Connect to database
-    conn = ps.connect(host=credentials['POSTGRES_ADDRESS'], database=credentials['POSTGRES_DBNAME'],
-                      user=credentials['POSTGRES_USERNAME'], password=credentials['POSTGRES_PASSWORD'],
+    # connect to database
+    conn = ps.connect(host=credentials['POSTGRES_ADDRESS'],
+                      database=credentials['POSTGRES_DBNAME'],
+                      user=credentials['POSTGRES_USERNAME'],
+                      password=credentials['POSTGRES_PASSWORD'],
                       port=credentials['POSTGRES_PORT'])
-    # Create cursor
+    # create cursor
     cur = conn.cursor()
 
-    # Cryptowatch API URL
+    # cryptowatch API URL
     base_url = ('https://api.cryptowat.ch/markets/{exchange}/{trading_pair}/'
-                'ohlc?apikey={api_key}&periods={period}&after={cutoff_time}')
+                'ohlc?apikey={api_key}&periods={period}')
 
-    # Cryptowatch api key
+    # cryptowatch api key
     api_key = credentials['API_KEY']
 
-    # Iterate through all exchange/trading pair combinations
+    # iterate through all exchange/trading pair combinations
     for exchange in exchanges:
         for trading_pair in exchanges[exchange]:
             for period in periods:
@@ -93,10 +67,6 @@ def insert_data(credentials, exchanges, periods=['300','3600']):
                 table_name = ('_'.join(exchange.split('-')) + '_' +
                               trading_pair)
                 
-                # use current time to calculate window for past 12 candles
-                now = round(time.time())
-                cutoff_time = str(now - (12 * int(period)))
-                
                 # cryptowatch wants the trading pair without the underscore
                 cleaned_trading_pair = trading_pair.replace('_', '')
 
@@ -104,8 +74,7 @@ def insert_data(credentials, exchanges, periods=['300','3600']):
                 url = base_url.format(exchange=exchange,
                                       trading_pair=cleaned_trading_pair,
                                       api_key=api_key,
-                                      period=period,
-                                      cutoff_time=cutoff_time)
+                                      period=period)
 
                 try:
                     # get response
@@ -114,9 +83,9 @@ def insert_data(credentials, exchanges, periods=['300','3600']):
 
                     # get timestamps for last 12 candles in database
                     cur.execute('''SELECT closing_time FROM {schema}.
-                                {table_name} order by closing_time desc limit
-                                12'''.format(schema=schema,
-                                             table_name=table_name))
+                                {table_name} order by closing_time desc
+                                '''.format(schema=schema,
+                                table_name=table_name))
                     results = cur.fetchall()
                     timestamps = [result[0] for result in results]
 

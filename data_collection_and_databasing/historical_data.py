93ac54718d580ffffa8f91d674c2db593d4b3455
nrvanwyck <49887859+nrvanwyck@users.noreply.github.com>
2019-11-21 13:21:35 -0500

    Adding code for getting Kraken/Gemini data
    Former-commit-id: bf4066327588d7119b7ffa6f767d89f13fb04b0c

--- data_collection_and_databasing/historical_data.py
+++ data_collection_and_databasing/historical_data.py
@@ -1,11 +1,13 @@
 # Before running this py file, you should have a folder named 'coinbase_pro',
-# a folder named 'bitfinex', and a folder named 'hitbtc' in your repository.
+# a folder named 'bitfinex', a folder named 'hitbtc', a folder named 'gemini',
+# and a folder named 'kraken' in your repository.
 # This py file will add 36 csvs to the 'coinbase_pro' folder totalling 
-# 159.6 MB, 42 csvs to the 'bitfinex' folder totalling 286 MB, and 36 csvs
-# to the 'hitbtc' folder totalling 256.2 MB, for a grand total of 114 csvs
-# across three folders totalling 701.8 MB. The csvs are created with their
-# indices; if you would like to ignore the index when importing a csv with
-# pandas.read_csv, add index_col=0 as a parameter.
+# 159.6 MB, 42 csvs to the 'bitfinex' folder totalling 286 MB, 36 csvs to
+# the 'hitbtc' folder totalling 256.2 MB, 16 csvs to the 'gemini' folder
+# totalling 4.2 MB, and 30 csvs to the 'kraken' folder totalling 10.4 MB
+# for a grand total of 160 csvs across five folders totalling 716.4 MB. The
+# csvs are created with their indices; if you would like to ignore the index 
+# when importing a csv with pandas.read_csv, add index_col=0 as a parameter.
 
 import requests
 import pandas as pd
@@ -568,14 +570,185 @@ def get_hitbtc_candles(trading_pairs=trading_pairs,
 
             # printing the dataframe to track progress
             print(df)
+
+# start of June 2019 in Unix time; earlier Cryptowatch data not available
+start_of_jun_2019 = 1559347200
+
+# the present time; we will gather Cryptowatch data until the present
+now = round(time.time())
+
+# insert Cryptowatch API key here
+api_key = 'INSERT_API_KEY_HERE'
+
+# function for grabbing Kraken and Gemini historical candles from Cryptowatch
+def get_cryptowatch_candles(exchanges=['kraken', 'gemini'],
+                            trading_pairs=trading_pairs,
+                            periods=periods,
+                            starting=start_of_jun_2019,
+                            ending=now):
+    """This function gets historical candle data from the Cryptowatch API.
+    The trading_pairs parameter takes a list of trading pairs, where the 
+    currencies in the pair are separated by an underscore and each currency is
+    represented by its lowercase symbol on cryptowatch. The periods parameter
+    takes a list of candle intervals in seconds. The starting parameter takes
+    a Unix timestamp for the start of the historical window. The ending 
+    parameter takes a Unix timestamp for the end of the historical window. 
+    The function creates a csv for each trading pair in the trading_pairs list
+    for each candle interval in the periods list. The csvs contain all of the
+    available candles from the time specified by the starting parameter to the
+    time specified by the ending parameter. The csvs are added to the 
+    kraken and gemini folders in the repository. The filenames take the format 
+    exchange_pair_interval.csv; kraken_btc_usd_3600.csv contains one hour
+    (3600 seconds) candles for the BTC/USD trading pair from Kraken. The
+    candles consist of six values: closing_time, open, high, low, close,
+    base_volume. The closing time is given as a Unix timestamp."""
+    
+    # looping through kraken and gemini...
+    for exchange in exchanges:
+
+        # printing exchange to track progress...
+        print("EXCHANGE", exchange)
+    
+        # requesting available currency pairs from the Cryptowatch API
+        response = requests.get('https://api.cryptowat.ch/markets/{exchange}'
+                                '?apikey={api_key}'
+                                .format(exchange=exchange, api_key=api_key)
+                               ).json()
+
+        # turning the response into a list of available currency pairs
+        exchange_pairs = [result['pair'] for result in response['result']]
+
+        # checking that list of desired trading_pairs against the list of
+        # pcurrency airs available on Cryptowatch; underscores need to be 
+        # dropped to make that comparison.
+        exchange_pairs = [pair for pair in trading_pairs if pair.replace(
+            '_', '') in exchange_pairs]
+
+        # limit to the number of candles returned by the Cryptowatch API with
+        # each API request; 500 is the maximum.
+        limit = 500
+
+        # looping through candle intervals
+        for period in periods:
+
+            # printing candle interval to track progress
+            print("CANDLE SIZE:", period)
+
+            # since Cryptowatch API takes candle intervals as strings
+            if period == (60*60):
+                interval = '3600'
+            elif period == (5*60):
+                interval = '300'
+
+            # looping through trading pairs
+            for trading_pair in exchange_pairs:
+
+                # printing trading pair to track progress
+                print("TRADING PAIR:", trading_pair)
+
+                # reformatting the trading pair for use with Cryptowatch API
+                cleaned_trading_pair = trading_pair.replace('_', '')
+
+                # our csv will have the following columns in the following order
+                columns = ['closing_time', 'open', 'high', 'low', 'close', 
+                           'base_volume']
+
+                # creating an empty dataframe to populate with candles
+                df = pd.DataFrame(columns=columns)
+
+                # number of iterations it will take to grab all candles of the
+                # specififed interval in the specified historical window, given
+                # the limit to the number of candles returned by the Cryptowatch 
+                # API with each request; this is equivalent to ceiling dividing
+                # the number of seconds in the historical window by the number
+                # of seconds covered by each API request
+                total_iterations = ((ending-starting)+(limit*period)
+                                   )//(limit*period)
+
+                # completed_iterations will be updated with each iteration
+                completed_iterations = 0
+
+                # cutoff_time will be updated with each iteration as its value 
+                # is always that of the most recent timestamp covered by our 
+                # current API request; our function begins by grabbing the most
+                # recent data in our specified historical window and moves 
+                # backwards in time, so cutoff_time is initially set to the end of
+                # our specified historical window and its value decreses as we
+                # make requests for earlier data
+                cutoff_time = ending - ((limit-1)*period)
+                
+                # i.e., while we're still requesting data within our specified
+                # historical window...
+                while cutoff_time > (starting - (limit*period)):
+
+                    try:
+
+                        # constructing url for our API request
+                        url = ('https://api.cryptowat.ch/markets/{exchange}/'
+                               '{trading_pair}/ohlc?apikey={api_key}&'
+                               'periods={period}&after={after}'.format(
+                                   exchange=exchange, 
+                                   trading_pair=cleaned_trading_pair,
+                                   api_key=api_key,
+                                   period=period,
+                                   after=cutoff_time))
+                        
+                        # making API request
+                        response = requests.get(url).json()
+                        
+                        # turning the response into a list of candles
+                        candles = response['result'][interval]
+
+                        # isolating the candle data we need for each candle
+                        candles = [candle[:6] for candle in candles]
+
+                        # reversing order so earlier candles are first
+                        candles.reverse()
+                        
+                        # creating a pandas dataframe from the candles
+                        to_append = pd.DataFrame(candles, columns=columns)
+
+                        # appending the dataframe just created from the most 
+                        # recent response to the dataframe that will contain all 
+                        # of the candles within our specified historical window
+                        df = df.append(to_append).reset_index(drop=True)
+
+                        # updating the cutoff time for the next API request; if we
+                        # are requesting 500 one hour candles, we want the cutoff
+                        # time to be 500 hours earlier next time around
+                        cutoff_time -= (limit*period)
+
+                        # updating the number of completed iterations
+                        completed_iterations += 1
+
+                        # tracking progress by printing the number of API requests
+                        # that still need to be made to generate the next csv
+                        print("ITERATIONS REMAINING:", 
+                              (total_iterations-completed_iterations))
+
+                    except:
+                        pass
+
+                # if we grabbed the same candle twice, we want to drop that
+                df = df.drop_duplicates(subset='closing_time')
+
+                # sorting the candles from latest to earliest
+                df = df.sort_values(by='closing_time').reset_index(drop=True)
+
+                # exporting the dataframe to a csv
+                df.to_csv(exchange + '/' + exchange + '_' + trading_pair + 
+                          '_' + str(period) + '.csv')
+
+                # printing the dataframe to track progress
+                print(df)
             
-# calling all three functions to get the historical candles we want; since 
+# calling all four functions to get the historical candles we want; since 
 # the Bitfinex API returns 5000 candles with each request, the 
 # get_bitfinex_candles function completes its task more quickly than the
 # get_hitbtc_candles function (the HitBTC API returns 1000 candles with
 # each request), or the get_coinbase_pro_candles function (the Coinbase Pro 
-# API only returns 300 candles with each request); calling the functions that
-# take less time to run first...
+# API only returns 300 candles with each request)...
 get_bitfinex_candles()
 get_hitbtc_candles()
 get_coinbase_pro_candles()
+get_cryptowatch_candles()
